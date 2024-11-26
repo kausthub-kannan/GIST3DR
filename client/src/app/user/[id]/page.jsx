13cb8256f72b5b6d97de2f8f82fb3499d98fc315
hanzalahwaheed <hanzalah.w@gmail.com>
2024-11-27 01:56:47 +0530

    added model renders basics

--- client/src/app/user/[id]/page.jsx
+++ client/src/app/user/[id]/page.jsx
@@ -7,39 +7,59 @@ import { Badge } from "../../../components/ui/badge";
 import useAuthStore from "@/stores/authStore";
 import { ThreeDots } from "react-loader-spinner";
 import { useAuth } from "@/hooks/useAuth";
-import {isTokenExpired, handleLogout} from "../../../hooks/useAuth";
-import { deleteCookie } from 'cookies-next';
+import { isTokenExpired, handleLogout } from "../../../hooks/useAuth";
+import { deleteCookie } from "cookies-next";
+import Screw3D from "@/components/models/screw-3d";
+import Bone3D from "@/components/models/bone-3d";
 
+// sample data from getPatient api
+// {
+//   "id": "2b794326-74d3-41e3-b577-af0bc562805b",
+//   "name": "caitlyn",
+//   "age": 25,
+//   "bone_density_gram_per_centimeter_sq": 144.15,
+//   "height_millimeter": 34.24,
+//   "width_millimeter": 15.19,
+//   "thickness_millimeter": 60.6,
+//   "area_millimeter_sq": 1818.69,
+//   "modal_urls": {
+//       "cancellous": "", (url_string)
+//       "nerve_canal": null
+//   },
+//   "gif_urls": {
+//       "cancellous": "", (url_string)
+//       "cortical": "", (url_string)
+//       "nerve_canal": null (url_string)
+//   }
+// }
 
-
-
-
-export default function User({ params }) {
+export default function User() {
   useAuth();
   const router = useRouter();
   const clearAuthData = useAuthStore((state) => state.clearAuthData);
-  const { id } = params; 
+  const id = router.query?.id;
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
   // const token = useAuthStore((state) => state.token);
 
   useEffect(() => {
-    if(isTokenExpired()){
+    if (isTokenExpired()) {
       if (typeof window !== "undefined") {
         localStorage.removeItem("token");
         localStorage.removeItem("user_email");
         localStorage.removeItem("user_first_name");
         localStorage.removeItem("user_last_name");
-        deleteCookie('token');
-    }
+        deleteCookie("token");
+      }
 
-    // Clear Zustand store
-    if (clearAuthData) clearAuthData();
+      // Clear Zustand store
+      if (clearAuthData) clearAuthData();
 
-    router.push("/sign-in");
-    console.log("User logged out successfully.");
+      router.push("/sign-in");
+      console.log("User logged out successfully.");
     }
 
     const fetchUser = async () => {
@@ -59,9 +79,11 @@ export default function User({ params }) {
   }, [id, token]);
 
   if (loading) {
-    return (<>
-      <ThreeDots height="80" width="80" color="white" ariaLabel="loading" />
-    </>);
+    return (
+      <>
+        <ThreeDots height="80" width="80" color="white" ariaLabel="loading" />
+      </>
+    );
   }
 
   if (!user) {
@@ -70,18 +92,27 @@ export default function User({ params }) {
 
   return (
     <div className="flex flex-col gap-4 w-full">
-    <div className="card flex gap-4 w-full border-2  rounded-md p-4 items-center">
-      <h1 className="text-2xl font-bold">{user?.name || "Abhinav Naman"}</h1>
-      <Badge className="card ">{user?.age || "22"} years old</Badge>
+      <div className="card flex gap-4 w-full border-2  rounded-md p-4 items-center">
+        <h1 className="text-2xl font-bold">{user?.name || "Abhinav Naman"}</h1>
+        <Badge className="card ">{user?.age || "22"} years old</Badge>
 
-      {/* <h3 className="text-sm text-gray-500">{user?.age || "22"} years old</h3> */}
+        {/* <h3 className="text-sm text-gray-500">{user?.age || "22"} years old</h3> */}
+      </div>
+      <div className="flex gap-4 w-full card rounded-md p-4 items-center h-[80vh]">
+        <div className="w-1/2 h-full">
+          Screw 3D Render
+          <Screw3D
+            screwHeight={user.height_millimeter}
+            screwRadius={user.width_millimeter}
+          />
+          {user.name} Screw Height: {user.height_millimeter}mm
+          <br />
+          {user.name} Screw Width: {user.width_millimeter}mm
+        </div>
+        <div className="w-1/2 h-full">
+          <Bone3D modelPath={user.modal_urls.cancellous} />
+        </div>
+      </div>
     </div>
-    <div className="flex gap-4 w-full card rounded-md p-4 items-center h-[80vh]">
-      {/* AR part */}
-      {/* <DemoAR /> */}
-    </div>
-  </div>
   );
 }
-
-
