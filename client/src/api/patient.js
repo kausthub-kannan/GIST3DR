9c57f4e243a4bd261f339bf761ac5d1f2ccd0956
hanzalahwaheed <hanzalah.w@gmail.com>
2024-11-27 01:45:57 +0530

    get 50 patients and render them

--- client/src/api/patient.js
+++ client/src/api/patient.js
@@ -1,35 +1,36 @@
 //api/patient.js
-import apiClient from './apiClient';
+import apiClient from "./apiClient";
 
-export const getAllPatients = (token) => 
-  apiClient.get('/patient', {
-    headers: { Authorization: `Bearer ${token}` }
+export const getAllPatients = (token) =>
+  apiClient.get("/patient", {
+    params: {
+      limit: 50,
+    },
+    headers: { Authorization: `Bearer ${token}` },
   });
 
-export const getPatient = (patientId, token) => 
+export const getPatient = (patientId, token) =>
   apiClient.get(`/patient/${patientId}`, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   });
 
-  export const createPatient = async (data, token) => {
-    console.log("Data sent to API:", data);
-    console.log("Token sent:", token);
-    return apiClient.post('/patient/create', data, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  };
-  
-  
+export const createPatient = async (data, token) => {
+  console.log("Data sent to API:", data);
+  console.log("Token sent:", token);
+  return apiClient.post("/patient/create", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
 
-export const updatePatient = (patientId, data, token) => 
+export const updatePatient = (patientId, data, token) =>
   apiClient.put(`/patient/${patientId}`, data, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   });
 
-export const deletePatient = (patientId, token) => 
+export const deletePatient = (patientId, token) =>
   apiClient.delete(`/patient/${patientId}`, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   });
